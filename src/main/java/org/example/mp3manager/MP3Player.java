f0fd93f5c65100b613633664624d66a0e83440fe
Omar Naghiyev <me@omarnaghiyev.com>
2025-11-16 06:09:46 -0500

    Make MyMP3Files and MP3Player interactive version stable

--- src/main/java/org/example/mp3manager/MP3Player.java
+++ src/main/java/org/example/mp3manager/MP3Player.java
@@ -1,13 +1,11 @@
 package org.example.mp3manager;
+
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-
 import javazoom.jl.player.Player;
-
 
 /**
  * A simple class to play mp3 files
- *
  */
 public class MP3Player {
 
@@ -16,88 +14,44 @@ public class MP3Player {
     // forward references to inner classes
     MP3Thread playerThread = null;
 
-    MP3Controller playerController = null;
-
     /**
      *  an inner class to play an mp3 file inside a Java thread
-     *
      */
-    private class MP3Thread
-	extends Thread
-    {
-	private String mp3filename;
-	private Player player; 
+    private class MP3Thread extends Thread {
+        private String mp3filename;
+        private Player player;
 
-	public MP3Thread(String filename) {
-	    super( "Playing " + filename );
-	    mp3filename = filename;
-	}
+        public MP3Thread(String filename) {
+            super("Playing " + filename);
+            mp3filename = filename;
+        }
 
-	public void close() {
-	    if (player != null) 
-		player.close(); 
-	}
+        public void close() {
+            if (player != null)
+                player.close();
+        }
 
-	public void run() {
-	    try {
-		FileInputStream     fis = new FileInputStream(mp3filename);
-		BufferedInputStream bis = new BufferedInputStream(fis);
+        public void run() {
+            try {
+                FileInputStream fis = new FileInputStream(mp3filename);
+                BufferedInputStream bis = new BufferedInputStream(fis);
 
-		player = new Player(bis);
-		player.play(); 
-		if( !stopped )
-		  System.out.println( "MP3Player: playbak finished; press the return key to continue" );
-	    }
-	    catch (Exception e) {
-		System.err.println(e);
-		System.err.println( "MP3Player: Cannot play: " + mp3filename);
-		System.out.println( "MP3Player: press the return key to continue" );
-	    }
-	}
-    }
-
-
-    /**
-     * an inner class to control (stop) the playback of an mp3 file
-     *
-     */
-    private class MP3Controller
-	extends Thread
-    {
-    	public void run() {
-            char ch;
-	    try {
-	       System.out.println( "MP3Player: press the return key at any time to quit playback" );
-	       ch = (char) System.in.read();
-	       stopped = true;
-	       playerThread.close();
-	    }
-	    catch (Exception e) {
-		System.err.println(e);
-	    }
+                player = new Player(bis);
+                player.play();
+                if (!stopped)
+                    System.out.println("MP3Player: playback finished");
+            } catch (Exception e) {
+                System.err.println(e);
+                System.err.println("MP3Player: Cannot play: " + mp3filename);
+            }
         }
     }
 
     // Play a given file
-    //
-    public void play( String filename ) {
-	stopped = false;
-	playerThread = new MP3Thread( filename );
-	playerThread.start();
-	System.out.println( "MP3Player: file " + filename + " is playing" );
-	playerController = new MP3Controller();
-	playerController.start();
+    public void play(String filename) {
+        stopped = false;
+        playerThread = new MP3Thread(filename);
+        playerThread.start();
+        System.out.println("MP3Player: file " + filename + " is playing");
     }
-
-    // Wait until the currently running file finishes, or is interrupted
-    //
-    public void waitForPlaybackFinish() {
-	try {
-	    playerController.join();
-	}
-	catch( InterruptedException ie ) {}
-    }
-
 }
-
-	
